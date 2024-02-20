921255ae74fb4e1b8172ab4d1003ce7177a554b4
heyjoshlee <lee45276@gmail.com>
2024-02-20 23:15:56 +0800

    Add bootstrap form

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -8,4 +8,26 @@ class DealsController < ApplicationController
     @deal = Deal.new
   end
 
+  def show
+    @deal = Deal.find(params[:id])
+  end
+
+  def create
+    puts deal_params
+    @deal = Deal.new(deal_params)
+    if @deal.save
+      redirect_to @deal
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def deal_params
+    params.require(:deal).permit(:name, :source, :rating, :status, :notes)
+  end
+
+
 end
